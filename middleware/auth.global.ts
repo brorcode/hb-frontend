15fe1165ae1f75ad815eef36ce37c03dbd8e5d16
Sergey <sbratyshov@gmail.com>
2024-08-19 13:16:28 +0300

    Update middlewares

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,12 +1,15 @@
+import { useCookie } from '#app';
+
 export default defineNuxtRouteMiddleware((to) => {
   const [user] = usePersistentState<User>('user');
+  const xsrf = useCookie('XSRF-TOKEN');
 
   const excludedRoutes = ['/', '/login']; // Define routes to exclude
   if (excludedRoutes.includes(to.path)) {
     return; // Skip middleware for these routes
   }
 
-  if (!user.value) {
+  if (!user.value || !xsrf.value) {
     return navigateTo('/login', { replace: true });
   }
 });
--- middleware/guest.ts
+++ middleware/guest.ts
@@ -1,8 +1,11 @@
+import { useCookie } from '#app';
+
 export default defineNuxtRouteMiddleware(() => {
   const config = useRuntimeConfig();
   const [user] = usePersistentState<User>('user');
+  const xsrf = useCookie('XSRF-TOKEN');
 
-  if (user.value) {
+  if (user.value && xsrf.value) {
     return navigateTo(config.public.homeUrl, { replace: true });
   }
 });
